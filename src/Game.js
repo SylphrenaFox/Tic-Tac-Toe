67b4dd3f1702475ea59757651016c5684abe3ab8
Mariya <quokka.mariya@gmail.com>
2025-02-18 10:49:10 +0300

    Поменяла логику компонентов

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,5 @@
 import styles from './Game.module.css';
 import { useEffect, useState } from 'react';
-import { WIN_PATTERNS } from './assets/WIN_PATTERNS';
 import { store } from './store';
 import { Field } from './components/Field';
 import { Information } from './components/Information';
@@ -14,36 +13,6 @@ export const Game = () => {
 		});
 		return () => unsubscribe();
 	}, []);
-	const { field, currentPlayer, isGameEnded } = state;
-
-	const checkWinner = (field) => {
-		return WIN_PATTERNS.some((pattern) => {
-			const [a, b, c] = pattern;
-			return field[a] && field[a] === field[b] && field[a] === field[c];
-		});
-	};
-
-	const handleCellClick = (index) => {
-		if (field[index] || isGameEnded) return;
-
-		const newField = [...field];
-		newField[index] = currentPlayer;
-		store.dispatch({ type: 'SET_FIELD', payload: newField });
-
-		if (checkWinner(newField)) {
-			store.dispatch({ type: 'IS_GAME_ENDED', payload: true });
-			return;
-		}
-
-		if (newField.every((cell) => cell)) {
-			store.dispatch({ type: 'IS_DRAW', payload: true });
-		} else {
-			store.dispatch({
-				type: 'SET_CURRENT_PLAYER',
-				payload: currentPlayer === 'X' ? '0' : 'X',
-			});
-		}
-	};
 
 	const handleResetButton = () => {
 		store.dispatch({ type: 'RESTART_GAME' });
@@ -51,7 +20,7 @@ export const Game = () => {
 
 	return (
 		<>
-			<Field handleCellClick={handleCellClick} />
+			<Field />
 			<Information />
 			<button className={styles.resetButton} onClick={handleResetButton}>
 				Начать заново
--- src/components/Field.js
+++ src/components/Field.js
@@ -1,10 +1,41 @@
 import styles from './Field.module.css';
 import { store } from '../store';
+import { WIN_PATTERNS } from '../utils/WIN_PATTERNS';
 import PropTypes from 'prop-types';
 
-export const Field = ({ handleCellClick }) => {
+export const Field = () => {
 	const state = store.getState();
-	const { field } = state;
+	const { field, currentPlayer, isGameEnded } = state;
+
+	const checkWinner = (field) => {
+		return WIN_PATTERNS.some((pattern) => {
+			const [a, b, c] = pattern;
+			return field[a] && field[a] === field[b] && field[a] === field[c];
+		});
+	};
+
+	const handleCellClick = (index) => {
+		if (field[index] || isGameEnded) return;
+
+		const newField = [...field];
+		newField[index] = currentPlayer;
+		store.dispatch({ type: 'SET_FIELD', payload: newField });
+
+		if (checkWinner(newField)) {
+			store.dispatch({ type: 'IS_GAME_ENDED', payload: true });
+			return;
+		}
+
+		if (newField.every((cell) => cell)) {
+			store.dispatch({ type: 'IS_DRAW', payload: true });
+		} else {
+			store.dispatch({
+				type: 'SET_CURRENT_PLAYER',
+				payload: currentPlayer === 'X' ? '0' : 'X',
+			});
+		}
+	};
+
 	return (
 		<div className={styles.field}>
 			{field.map((cell, index) => {
